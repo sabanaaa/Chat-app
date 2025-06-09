7b7af7b3a53d8bb803eee8155eae94207b0c6c5f
sabana <messyme154@gmail.com>
2025-06-09 23:41:19 +0530

    s

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import cors from "cors";
- import path from "path";
+
+import path from "path";
+
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -11,8 +13,8 @@ import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const PORT=process.env.PORT;
-const __dirname=path.resolve();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,17 +24,19 @@ app.use(
     credentials: true,
   })
 );
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-server.listen(PORT, () =>{
-  console.log("Server is running on PORT:" + PORT);
+
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
